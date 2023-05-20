3aa2f917de6e048292cbf920fbc998feeb15a2dd
Hasan Ali Caliskan <hasanali7295@gmail.com>
2023-05-20 16:21:49 +0300

    added audio sources

--- Castle Mystery/Assets/Scripts/MainMenu.cs
+++ Castle Mystery/Assets/Scripts/MainMenu.cs
@@ -3,6 +3,13 @@ using UnityEngine.SceneManagement; // to switch between scenes
 
 public class MainMenu : MonoBehaviour
 {
+    public AudioSource audioSource;
+
+    public void PlayClickedSound()
+    {
+        audioSource.Play();
+    }
+
     public void GoToScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
